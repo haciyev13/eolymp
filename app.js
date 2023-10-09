c750c28f3eea144d3b03ab8592bca069a41a3cac
haciyev13 <ehedhaciyev001@gmail.com>
2023-10-10 01:25:25 +0400

    Add

---
+++ app.js
@@ -0,0 +1,362 @@
+//! 8800
+
+let str = "Hello, Phyton!";
+// console.log(str);
+
+//! 8801
+
+// let num = prompt("Reqem daxil edin");
+// console.log(num +1);
+
+//! 8802
+
+// let num = prompt("Reqem daxil edin");
+// console.log(num -1);
+
+//! 8803
+
+// let num = prompt("Reqem daxil edin");
+// console.log(num + 1);
+
+//! 8804
+
+// let num = prompt("Reqem daxil edin");
+// let num2 = prompt("Reqem daxil edin");
+
+// console.log(num + num2);
+
+//! 8805
+
+// let num = prompt("Reqem daxil edin");
+// let num2 = prompt("Reqem daxil edin");
+
+// console.log(num + num2);
+
+//! 8806
+
+// let a = 9;
+// let b = 12;
+
+// console.log(num + num2);
+
+//! 8807
+
+// let number = prompt("Reqeminizi daxil edin");
+
+// number = -number;
+// console.log(number);
+
+//! 8808
+
+// let num1 = prompt("Num1 i daxil et");
+// let num2 = prompt("Num2 i daxil et");
+
+// console.log(num1 - num2);
+
+//! 8809
+
+// let num = [12, 9];
+// let sum = 0;
+// for (let i = 0; i < num.length; i++) {
+//   sum += num[i];
+// }
+
+// console.log(sum);
+
+// //! 8810
+
+// let a = 13;
+// let b = 9;
+// let c = 5;
+
+// let sum = a - c + (b - c) + c;
+// console.log(sum);
+
+// //! 8811
+
+// let a = 3;
+// let b = 7;
+
+// let sum = a * b;
+// console.log(sum);
+
+// //! 8812
+
+// let a = 3;
+// let b = 7;
+
+// let perimetr = 2 * (a + b);
+// let sahe = (a * b);
+// console.log(perimetr, sahe);
+
+//! 8813
+
+// let a = 2;
+// let b = 3;
+// let c = 4;
+
+// let hecm = 2 * (a * b + b * c + a * c);
+// let sahe = (a * b * c);
+// console.log(hecm, sahe);
+
+//! 8814
+
+// let a = 3;
+
+// let p = 4 * a;
+// let s = Math.pow(a , 2);
+// console.log(p, s);
+
+//! 8815
+
+// let a = 3;
+
+// let s = 6 * Math.pow(a, 2);
+// let p =   Math.pow(a, 3);
+// console.log( s, p );
+
+//! 8816
+
+// let a = 2;
+// console.log(Math.pow(a, 10));
+
+//! 8817
+
+// let n = prompt("reqemi daxil et");
+
+// if (n == 1) {
+//   for (let i = 1; i <= 9; i++) {
+//     console.log(i);
+//   }
+// } else if (n == 2) {
+//   for (let i = 10; i <= 99; i++) {
+//     console.log(i);
+//   }
+// } else if (n == 3) {
+//   for (let i = 100; i <= 999; i++) {
+//     console.log(i);
+//   }
+// }
+
+//! 8818
+
+// let n = prompt("Reqem daxil et");
+
+// for (let i = 1; i <= 9; i++) {
+//   if (n == 1 && i % 2 != 0) {
+//     console.log(i.length);;
+//   }
+// }
+// for (let i = 10; i <= 99; i++) {
+//   if (n == 2 && i % 2 != 0) {
+//     console.log(i.length);
+//   }
+// }
+// for (let i = 100; i <= 999; i++) {
+//   if (n == 3 && i % 2 != 0) {
+//     console.log(i.length);
+//   }
+// }
+
+//! 8819
+
+
+// let n = prompt("Reqem daxil et");
+
+// for (let i = 1; i <= 9; i++) {
+//   if (n == 1 && i % 2 == 0) {
+//     console.log(i.length);;
+//   }
+// }
+// for (let i = 10; i <= 99; i++) {
+//   if (n == 2 && i % 2 == 0) {
+//     console.log(i.length);
+//   }
+// }
+// for (let i = 100; i <= 999; i++) {
+//   if (n == 3 && i % 2 == 0) {
+//     console.log(i.length);
+//   }
+// }
+
+//! 8820
+
+// let n = prompt("Reqem daxil et");
+
+// for (let i = 1; i <= 9; i++) {
+//   if (n == 1 && i % 2 != 0) {
+//     console.log(i.length);;
+//   }
+// }
+// for (let i = 10; i <= 99; i++) {
+//   if (n == 2 && i % 2 != 0) {
+//     console.log(i.length);
+//   }
+// }
+// for (let i = 100; i <= 999; i++) {
+//   if (n == 3 && i % 2 != 0) {
+//     console.log(i.length);
+//   }
+// }
+
+//! 8821
+
+// let n = prompt("Reqem daxil et");
+
+// for (let i = 1; i <= 9; i++) {
+//   if (n == 1 && i % 2 == 0) {
+//     console.log(i.length);
+//   }
+// }
+// for (let i = 10; i <= 99; i++) {
+//   if (n == 2 && i % 2 == 0) {
+//     console.log(i.length);
+//   }
+// }
+// for (let i = 100; i <= 999; i++) {
+//   if (n == 3 && i % 2 == 0) {
+//     console.log(i.length);
+//   }
+// }
+
+
+//! 8822
+
+// let n = prompt("Reqem daxil et");
+
+// for (let i = 1; i <= 9; i++) {
+//   if (n == 1 && i !== 6) {
+//     console.log(i.length);
+//   }
+// }
+// for (let i = 10; i <= 99; i++) {
+//   if (n == 2 && i !== 6) {
+//     console.log(i.length);
+//   }
+// }
+// for (let i = 100; i <= 999; i++) {
+//   if (n == 3 && i !== 6) {
+//     console.log(i.length);
+//   }
+// }
+
+
+
+//! 8823  ????????????????????????????????????
+
+// let n = prompt("Reqem daxil et");
+
+// for (let i = 1; i <= 9; i++) {
+//   if (n == 1 && i == 7) {
+//     console.log(i.length);
+//   }
+// }
+// for (let i = 10; i <= 99; i++) {
+//   if (n == 2 && i === 7) {
+//     console.log(i.length);
+//   }
+// }
+// for (let i = 100; i <= 999; i++) {
+//   if (n == 3 && i === 7) {
+//     console.log(i.length);
+//   }
+// }
+
+
+//! 8824
+
+function number(arr) {
+  for (let i = arr.length - 1; i >= 1; i--) {
+    if (arr[i] - arr[i - 1] != 1) {
+      return arr[i] - 1;
+    }
+  }
+}
+
+// console.log(number([6, 8]));
+
+function numberMenfi(arr) {
+  for (let i = arr.length - 1; i >= 1; i--) {
+    if (arr[i] - arr[i - 1] != 1) {
+      return arr[i] - -1;
+    }
+  }
+}
+
+// console.log(numberMenfi([-16, -18]));
+// console.log(numberMenfi([-14, -16]));
+
+//! 8825
+// function arithmetic(x) {
+//   return Math.pow(x, 3) - (5 * Math.pow(x, 2)) / 7 + 9 * x - 3 / x + 1;
+// }
+// console.log(+arithmetic(1).toFixed(3));
+
+//! 8826
+
+// function arithmetic(x) {
+//   return x - (Math.pow(x, 2) + 4) / 2 + Math.pow(x, 3) - 3 / (x + 7);
+// }
+// console.log(+arithmetic(1).toFixed(3));
+
+//! 8827
+
+// function arithmetic(x) {
+//   return (Math.pow(x, 2) + 3 * x - 4) / (2 * x - 3) - (x + 2) / (Math.pow(x, 2) - 5 * x + 7)
+// }
+// console.log(+arithmetic(1).toFixed(3));
+
+//! 8828
+
+// function arithmetic(x) {
+//   return 2 * x - 1 / Math.pow(x, 2) + Math.sqrt(Math.pow(x, 2) + 1) / 2;
+// }
+// console.log(+arithmetic(1).toFixed(3));
+
+//! 8829
+
+// function sumis(x) {
+//   return ((2 * x) / (Math.sqrt(Math.pow(x, 2) + 1))) - ((Math.sqrt(Math.pow(x, 2) + 1)) / (Math.pow(x, 3)) )
+// }
+// console.log(sumis(1));
+
+//! 8830
+
+// function sumis(x) {
+//   return (
+//     Math.sqrt(Math.pow(x, 4) + 1) / Math.pow(x, 2) -
+//     Math.sqrt(Math.pow(x, 2) / (Math.pow(x, 2) + 1))
+//   );
+// }
+// console.log(+sumis(1).toFixed(3));
+
+//! 8831
+
+// function sumis(x, y) {
+//   return 2 * Math.pow(x, 2) - (4 * x * y) + (3 * Math.pow(y, 2)) + (x + y) / 7;
+// }
+// console.log(+sumis(1, -2).toFixed(3));
+
+//! 8832
+
+// function sumis(x, y) {
+//     return (Math.pow(x, 2) - (2 * x * y) + 4 * Math.pow(y, 2)) / (x + 5) + (3 * Math.pow(x, 2) - Math.pow(y, 2)) / (y- 7)
+// }
+// console.log(+sumis(1, -2).toFixed(3));
+
+//! 8833
+
+// function sumis(x, y) {
+//     return ((2 * x) + y) / (Math.pow(x, 2) - (x*y) + (4* Math.pow(y, 2))) + (2 * Math.pow(x, 2)- (x*y) + Math.pow(y, 2)) / (x + 4 * y)
+// }
+// console.log(+sumis(1, -2).toFixed(3));
+
+//! 8834
+
+// function sumis(x, y) {
+//   return (
+//     (2 * x * y) / Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) -
+//     Math.pow(x + y - 1, 2) / (x * y)
+//   );
+// }
+// console.log(+sumis(1, -2).toFixed(3));
